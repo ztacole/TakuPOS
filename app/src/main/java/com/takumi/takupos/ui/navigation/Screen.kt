e6e351ca54d8556647f56f9679a73cbac989eac3
Jeki <ahmadzaqi490@gmail.com>
2025-03-27 17:31:46 +0700

    add new screen

--- app/src/main/java/com/takumi/takupos/ui/navigation/Screen.kt
+++ app/src/main/java/com/takumi/takupos/ui/navigation/Screen.kt
@@ -3,4 +3,5 @@ package com.takumi.takupos.ui.navigation
 sealed class Screen(val route: String){
     data object Home: Screen("home")
     data object History: Screen("history")
+    data object Scan: Screen("Scan")
 }
